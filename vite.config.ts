db92494903b1a44dc03678187897923d364ae3fb
Pedro Rubinger <pedro.rubinger@gmail.com>
2024-05-12 12:54:26 -0300

    chore: set up relative paths imports

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      src: '/src',
+      components: '/src/components',
+      pages: '/src/pages',
+      interfaces: '/src/interfaces',
+      utils: '/src/utils',
+      assets: '/src/assets',
+      services: '/src/services',
+    },
+  },
 })
