a6bda7824314824f6db08b7a6cb9ceab1c1e8773
Pedro Rubinger <pedro.rubinger@gmail.com>
2024-05-12 19:11:37 -0300

    feat: add Button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,28 @@
+import { ButtonHTMLAttributes } from 'react'
+
+import { ButtonSize, ButtonVariant } from 'src/interfaces/button'
+import { IconImg, StyledButton } from 'src/components/Button/styles'
+
+interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode
+  /** @default 'sm' */
+  size?: ButtonSize
+  /** @default 'primary' */
+  variant?: ButtonVariant
+  iconSrc?: string
+}
+
+export const Button: React.FC<Props> = ({
+  children,
+  iconSrc,
+  size = 'sm',
+  variant = 'primary',
+  ...rest
+}) => {
+  return (
+    <StyledButton variant={variant} size={size} {...rest}>
+      {!!iconSrc && <IconImg src={iconSrc} />}
+      {children}
+    </StyledButton>
+  )
+}
---
+++ src/components/Button/styles.ts
@@ -0,0 +1,79 @@
+import styled from 'styled-components'
+
+import { Sizes } from 'src/styles/tokens/sizes'
+import { ButtonSizes } from 'src/components/Button/variants'
+import { ButtonSize, ButtonVariant } from 'src/interfaces/button'
+import { Theme } from 'src/interfaces/theme'
+import { Colors } from 'src/styles/tokens/colors'
+
+export const Box = styled.div`
+  background-color: ${({ theme }) => theme.headerBackgroundColor};
+
+  display: flex;
+  justify-content: space-between;
+
+  padding: ${Sizes.md}px;
+`
+
+interface ButtonProps {
+  size: ButtonSize
+  variant: ButtonVariant
+}
+
+const getBackgroundColor = (theme: Theme, variant: ButtonVariant) => {
+  if (variant === 'outlined') return theme.buttonOutlinedBackgroundColor
+  if (variant === 'secondary') return theme.buttonSecondaryBackgroundColor
+
+  return theme.buttonPrimaryBackgroundColor
+}
+
+const getBorderColor = (theme: Theme, variant: ButtonVariant) => {
+  if (variant === 'outlined') return theme.buttonOutlinedBorderColor
+}
+
+const getBorderWidth = (variant: ButtonVariant) => {
+  if (variant === 'outlined') return '1px'
+
+  return '0px'
+}
+
+const getColor = (theme: Theme, variant: ButtonVariant) => {
+  if (variant === 'outlined') return theme.buttonOutlinedColor
+  if (variant === 'secondary') return theme.buttonSecondaryColor
+
+  return theme.buttonPrimaryColor
+}
+
+const getBackgroundColorHover = (theme: Theme, variant: ButtonVariant) => {
+  if (variant === 'outlined') return theme.buttonOutlinedBackgroundColor
+
+  return Colors.blue800
+}
+
+export const StyledButton = styled.button<ButtonProps>`
+  font-size: ${({ size = 'sm' }) => ButtonSizes[size].fontSize}px;
+
+  padding: ${({ size = 'sm' }) =>
+    `${ButtonSizes[size].paddingVertical}px ${ButtonSizes[size].paddingHorizontal}px`};
+
+  border-color: ${({ theme, variant }) => getBorderColor(theme, variant)};
+  border-radius: ${({ size = 'sm' }) => ButtonSizes[size].borderRadius}px;
+  border-width: ${({ variant }) => getBorderWidth(variant)};
+
+  color: ${({ theme, variant }) => getColor(theme, variant)};
+  background-color: ${({ theme, variant }) =>
+    getBackgroundColor(theme, variant)};
+
+  transition: 0.6s;
+
+  &:hover {
+    cursor: pointer;
+
+    background-color: ${({ theme, variant }) =>
+      getBackgroundColorHover(theme, variant)};
+  }
+`
+
+export const IconImg = styled.img`
+  margin-right: ${Sizes.sm}px;
+`
