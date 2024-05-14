05dc6b7947152d3c68199e39909bbf53786a4381
Pedro Rubinger <pedro.rubinger@gmail.com>
2024-05-14 18:56:49 -0300

    fix: adjust typo

--- src/services/assets/index.ts
+++ src/services/assets/index.ts
@@ -4,14 +4,14 @@ import { Api } from 'src/services/api'
 import { handleError } from 'src/utils/helpers/error'
 
 interface GetAssetsParams {
-  assetId: string
+  companyId: string
 }
 
 export const getAssets = async ({
-  assetId,
+  companyId,
 }: GetAssetsParams): Promise<ApiServiceResponse<Asset[]>> => {
   try {
-    const { data } = await Api.get<Asset[]>(`/companies/${assetId}/assets`)
+    const { data } = await Api.get<Asset[]>(`/companies/${companyId}/assets`)
 
     return { success: true, data }
   } catch (err) {
