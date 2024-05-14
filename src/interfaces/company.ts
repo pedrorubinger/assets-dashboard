399bfa7467126edb975c6d3d7d9bbff946e87954
Pedro Rubinger <pedro.rubinger@gmail.com>
2024-05-14 18:35:46 -0300

    feat: add CompanyLocation and CompanyAsset types

--- src/interfaces/company.ts
+++ src/interfaces/company.ts
@@ -2,3 +2,13 @@ export interface Company {
   id: string
   name: string
 }
+
+export interface CompanyLocation {
+  id: string
+  name: string
+  parentId: string | null
+}
+
+export interface CompanyAsset {
+  id: string
+}
