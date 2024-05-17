a4c75910b4449785f28771b84160b5ae7135d4ff
Pedro Rubinger <pedro.rubinger@gmail.com>
2024-05-17 00:05:03 -0300

    style: add the initial structure of asset list styles

---
+++ src/pages/dashboard/components/AssetTree/styles.ts
@@ -0,0 +1,55 @@
+import styled from 'styled-components'
+
+import { Sizes } from 'src/styles/tokens/sizes'
+import { Colors } from 'src/styles/tokens/colors'
+
+export const List = styled.ul`
+  padding: 0;
+  margin: 0;
+`
+
+interface ListItemProps {
+  $isRoot?: boolean
+}
+
+export const ListItem = styled.li<ListItemProps>`
+  margin: ${Sizes.xsm2}px 0;
+  margin-left: ${({ $isRoot }) => ($isRoot ? 0 : Sizes.md2)}px;
+
+  list-style: none;
+`
+
+interface ItemLineProps {
+  $isSelected?: boolean
+}
+
+export const ItemLine = styled.div<ItemLineProps>`
+  display: flex;
+  gap: ${Sizes.sm}px;
+  align-items: center;
+
+  padding: ${Sizes.xsm}px;
+
+  cursor: pointer;
+
+  background-color: ${({ $isSelected }) =>
+    $isSelected ? Colors.blue500 : 'transparent'};
+`
+
+interface ListTextProps {
+  $isSelected?: boolean
+}
+
+export const ListText = styled.span<ListTextProps>`
+  font-size: ${Sizes.sm3}px;
+  font-weight: 400;
+  text-align: left;
+  color: ${({ $isSelected }) => ($isSelected ? Colors.white : Colors.gray850)};
+`
+
+export const ChevronIcon = styled.img`
+  margin-left: ${Sizes.xsm}px;
+
+  width: ${Sizes.sm1}px;
+  height: ${Sizes.sm1}px;
+`
